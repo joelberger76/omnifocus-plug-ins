ffb8bb182e59e5e4138fdb4b0ef110c51bb923e5
Joel Berger <joelberger@gmail.com>
2020-08-29 14:52:44 -0400

    Added ctags support

--- bin/Deploy OmniFocus Plug-Ins.sh
+++ bin/Deploy OmniFocus Plug-Ins.sh
@@ -4,3 +4,5 @@ rsync -a --exclude .git/ ~/Code/omnifocus-plug-ins/plug-ins/ ~/Library/Mobile\ D
 
 ## Ensure OmniFocus reloads packages without restarting the app
 touch ~/Library/Mobile\ Documents/iCloud\~com\~omnigroup\~OmniFocus/Documents/Plug-Ins/*.omnifocusjs
+
+ctags --excmd=number --tag-relative=no --fields=+a+m+n+S -f /Users/joel/Library/Application\ Support/BBEdit/Completion\ Data/JavaScript/omnifocus-plug-ins.tags -R /Users/joel/Code/omnifocus-plug-ins/plug-ins >/dev/null 2>&1
